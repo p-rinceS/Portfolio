295c64f55081cea913e051208eba3a6e6ee3d5a0
Prince Sonani <princesonani9112@gmail.com>
2022-12-05 23:28:22 -0600

    still trying to make circle move

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
         // this is my first java script code
+        let myDiv = document.getElementById("cursor");
+
+        let div = document.getElementById("X-Value");
+        let div2 = document.getElementById("Y-Value");
 
         window.addEventListener('mousemove', function(e){
                 
-                this.document.getElementById('X-Value').textContent = e.x;
-                this.document.getElementById('Y-Value').textContent = e.y;
-        })
+               var x = e.x;
+               var y = e.y;
+                
+               div.textContent = e.x;
+               div2.textContent = e.y;
+               myDiv.style.left = x + "px";
+               myDiv.style.top = y + "px";
+        });

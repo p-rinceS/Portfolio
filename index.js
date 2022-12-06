86ae95d548860945739eb3f94f6a738df39a293a
Prince Sonani <princesonani9112@gmail.com>
2022-12-05 22:56:50 -0600

    testing mouse movement

---
+++ index.js
@@ -0,0 +1,7 @@
+        // this is my first java script code
+
+        window.addEventListener('mousemove', function(e){
+                
+                this.document.getElementById('X-Value').textContent = e.x;
+                this.document.getElementById('Y-Value').textContent = e.y;
+        })

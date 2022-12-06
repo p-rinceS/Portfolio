af61659f0b98e77c023dce6e0a8356b854811130
Prince Sonani <princesonani9112@gmail.com>
2022-12-06 00:41:39 -0600

    click sound :)

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
         let div2 = document.getElementById("Y-Value");
 
         document.addEventListener('mousemove', function(e){
-                
+                var onmousestop = function() {
+                        console.log("test");
+                      }, thread;
                 
                var x = e.x;
                var y = e.y;
@@ -14,4 +16,19 @@
                div2.textContent = e.clientY;
                cursor.style.left = x + "px";
                cursor.style.top = y + "px";
+               cursor.style.color
+
+  
+        });
+
+        // cursor transparency
+        
+        var audio = new Audio("old_click_sound.mp3");
+
+        addEventListener('click', function(event){
+
+                audio.play();
+                cursor.style.opacity = 1;
+                setTimeout(() => {cursor.style.opacity = .5;}, 100);
+
         });

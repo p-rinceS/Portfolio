0eab31458c3e262c88d4b70f43bef0b5962cf1de
Prince Sonani <princesonani9112@gmail.com>
2022-12-07 01:44:20 -0600

    added activity status

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-        // this is my first java script code
-        let cursor = document.getElementById('cursor');
+// this is my first java script code
+let cursor = document.getElementById("cursor");
 
-        let div = document.getElementById("X-Value");
-        let div2 = document.getElementById("Y-Value");
+const REFRESH_INTERVAL = 10;     
 
-        document.addEventListener('mousemove', function(e){
-                var onmousestop = function() {
-                        console.log("test");
-                      }, thread;
-                
-               var x = e.x;
-               var y = e.y;
-                
-               div.textContent = e.clientX;
-               div2.textContent = e.clientY;
-               cursor.style.left = x + "px";
-               cursor.style.top = y + "px";
+let div = document.getElementById("X-Value");
+let div2 = document.getElementById("Y-Value");
 
-        });
+document.addEventListener("mousemove", function (e) {
+  var onmousestop = function () {
+      console.log("test");
+    },
+    thread;
 
-        // cursor transparency
-        // and click audio
+  var x = e.x;
+  var y = e.y;
 
-        // change if you want to change clickSound"
-        var clickSound = new Audio("old_click_sound.mp3");
+  div.textContent = e.clientX;
+  div2.textContent = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+  cursor.style.opacity = 1;
 
-        addEventListener('click', function(event){
+  setTimeout(() => {
+    cursor.style.opacity = 0;
+  }, 200);
+});
 
-                clickSound.play();
-                cursor.style.opacity = 0;
-                setTimeout(() => {cursor.style.opacity = 1;}, 150);
+// cursor transparency
+// and click audio
 
-        });
+// change if you want to change clickSound"
+var clickSound = new Audio("old_click_sound.mp3");
+
+addEventListener("click", function (event) {
+  clickSound.play();
+  cursor.style.opacity = 0;
+  setTimeout(() => {
+    cursor.style.opacity = 1;
+  }, 150);
+});
+
+
+getActivity();
+
+
+
+
+async function getActivity(){
+        const api_url = "https://api.lanyard.rest/v1/users/135932078486192128";
+        const response = await fetch(api_url);
+        const stuff = await response.json();
+        const result = JSON.stringify(stuff);
+        console.log(stuff.data.discord_status);
+        let activityCirc = document.getElementById('activity');
+        if (stuff.data.discord_status == "offline"){
+                activityCirc.style.backgroundColor = '#727672';
+        }
+        if(stuff.data.discord_status == "dnd"){
+                activityCirc.style.backgroundColor = '#b0251d';
+        }
+        if(stuff.data.discord_status == "online"){
+                activityCirc.style.backgroundColor = '#2ec21d';
+        }
+        if(stuff.data.discord_status == "idle"){
+                activityCirc.style.backgroundColor = '#d3ae19';
+        }
+}
+
+setInterval(()=>{ // send a request to server every 5 seconds (REFRESH_INTERVAL)
+        getActivity()
+}, REFRESH_INTERVAL * 1000)
